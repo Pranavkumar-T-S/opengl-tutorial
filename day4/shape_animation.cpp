607db84664794c2017c85fa65649cdffbb73b356
pranavkumar-t-s <pranavtspk@gmail.com>
2023-01-24 19:53:21 +0530

    Format and Add minor changes

--- day4/shape_animation.cpp
+++ day4/shape_animation.cpp
@@ -2,28 +2,30 @@
 
 #include "opengl_template.h"
 #include <cmath>
-  std::string vertexShader =
-        "#version 400\n"
-        "\n"
-        "in vec4 position;"
-        "uniform vec2 translation; "
-        "\n"
-        "void main()\n"
-        "{\n"
-        " gl_Position = position + vec4(translation, 0.0f, 0.0f);\n"
-        "}\n";
+std::string vertexShader =
+    "#version 400\n"
+    "\n"
+    "in vec4 position;"
+    "uniform vec2 translation; "
+    "uniform vec3 inp_color;"
+    "out vec3 i_color;"
+    "\n"
+    "void main()\n"
+    "{\n"
+    " gl_Position = position + vec4(translation, 0.0f, 0.0f);\n"
+    "i_color = inp_color;"
+    "}\n";
 
-    std::string fragmentShader =
-        "#version 400\n"
-        "\n"
-        "out vec4 color;"
-        "uniform vec3 inp_color;"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "  color = vec4(inp_color, 1.0f);\n"
-        "}\n";
-
+std::string fragmentShader =
+    "#version 400\n"
+    "\n"
+    "out vec4 color;"
+    "in vec3 i_color;"
+    "\n"
+    "void main()\n"
+    "{\n"
+    "  color = vec4(i_color, 1.0f);\n"
+    "}\n";
 
 int translate[2] = {0, 0};
 int color[3] = {10, 10, 10};
@@ -41,35 +43,37 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
     if (key == GLFW_KEY_A && action == GLFW_PRESS)
         translate[0]--;
     if (key == GLFW_KEY_J && action == GLFW_PRESS)
-        color[0] = ((color[0]<1 ? 10 :color[0]) - 1)%10;
+        color[0] = ((color[0] < 1 ? 10 : color[0]) - 1) % 10;
     if (key == GLFW_KEY_K && action == GLFW_PRESS)
-        color[1] = ((color[1]<1 ? 10 :color[1]) - 1)%10;
+        color[1] = ((color[1] < 1 ? 10 : color[1]) - 1) % 10;
     if (key == GLFW_KEY_L && action == GLFW_PRESS)
-        color[2]= ((color[2]<1 ? 10 :color[2]) - 1)%10;
+        color[2] = ((color[2] < 1 ? 10 : color[2]) - 1) % 10;
     if (key == GLFW_KEY_U && action == GLFW_PRESS)
-        color[0]= (color[0] + 1)%10;
+        color[0] = (color[0] + 1) % 10;
     if (key == GLFW_KEY_I && action == GLFW_PRESS)
-        color[1]= (color[1] + 1)%10;
+        color[1] = (color[1] + 1) % 10;
     if (key == GLFW_KEY_O && action == GLFW_PRESS)
-        color[2]= (color[2] + 1)%10;
+        color[2] = (color[2] + 1) % 10;
 }
 
-int main(){
+int main()
+{
     std::string a = "OpenGL window";
     OpenGLContext opengl = OpenGLContext(a);
-    if(!opengl.isInitiated){
+    if (!opengl.isInitiated)
+    {
         std::cout << " Unable to init()" << std::endl;
         return -1;
     }
-   // generate vertex points
+    // generate vertex points
     float r = 0.5;
     int n;
     std::cout << " Enter the number of points to be generated: " << std::endl;
     std::cin >> n;
     std::cout << "Press 'wasd' to translate polygon and 'jkl' to decrease colors and 'uio' to increase colors" << std::endl;
     // Generating vertex coefficients and index array
-    float *arr = new float[2*n];
-    unsigned int *indices =  new unsigned int[n];
+    float *arr = new float[2 * n];
+    unsigned int *indices = new unsigned int[n];
     for (int i = 0; i < n; i++)
     {
         arr[2 * i] = r * cosf(2 * M_PI * i / n);
@@ -81,7 +85,6 @@ int main(){
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &ebo);
 
-
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBindVertexArray(vao);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
@@ -92,19 +95,19 @@ int main(){
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void *)0);
     glEnableVertexAttribArray(0);
     opengl.setCallback(key_callback);
-    static unsigned int id =  CreateShader(vertexShader, fragmentShader);
+    static unsigned int id = CreateShader(vertexShader, fragmentShader);
     glUseProgram(id);
 
-     while (!glfwWindowShouldClose(opengl.window))
+    while (!glfwWindowShouldClose(opengl.window))
     {
 
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        
+
         int translation = glGetUniformLocation(id, "translation");
-        glUniform2f(translation, 1.0 * translate[0] / 10, 1.0 * translate[1] / 10);
+        glUniform2f(translation, translate[0] / 10.0, translate[1] / 10.0);
         int inp_color = glGetUniformLocation(id, "inp_color");
-        glUniform3f(inp_color, 1.0 * color[0] / 10, 1.0 * color[1] / 10, 1.0 * color[2] / 10);
+        glUniform3f(inp_color, color[0] / 10.0, color[1] / 10.0, color[2] / 10.0);
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
         glDrawElements(GL_TRIANGLE_FAN, n, GL_UNSIGNED_INT, 0);
@@ -112,8 +115,7 @@ int main(){
         /* Swap front and back buffers */
         glfwSwapBuffers(opengl.window);
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        /* Wait for and process events */
+        glfwWaitEvents();
     }
-
 }
