9c25cf5f76d6ff479a5dcbe768a3c4f66ad7a6b3
pranavkumar-t-s <pranavtspk@gmail.com>
2023-02-28 00:18:20 +0530

    Add #include chrono and define M_PI

--- opengl_template.h
+++ opengl_template.h
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #define M_PI 3.14159265358979323846
+#include <chrono>
+#define uint unsigned int
+
 int CreateShader(const std::string &, const std::string &);
 class OpenGLContext
 {
